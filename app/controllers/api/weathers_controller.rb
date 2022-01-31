35c6bcb342f55f0fad4669710a890110a59851b2
shushikata <shu.42111227@gmail.com>
2022-01-31 19:48:25 +0900

    天気情報を取得

--- app/controllers/api/weathers_controller.rb
+++ app/controllers/api/weathers_controller.rb
@@ -1,21 +1,48 @@
 class Api::WeathersController < ApplicationController
+  # skip_before_action :authenticate_user!
+  # skip_before_action :verify_authenticity_token
+
   def index
     weather_response = Faraday.get("https://www.jma.go.jp/bosai/forecast/data/forecast/#{area_code}.json")
     weather_response_json = JSON.parse(weather_response.body)
-    weekly_weather_area = weather_response_json[1]['timeSeries'][0]['areas'].first # area: ['東京', '伊豆諸島北部'...]
-    weekly_weather_area.delete('reliabilities')
-    render json: weekly_weather_area
+    weekly_weather = weather_response_json[1]['timeSeries'][0]
+    weekly_dates = weekly_weather['timeDefines']
+
+    local_weekly_weather = weekly_weather['areas'][0]
+    local_weekly_weather.delete('reliabilities')
+    local_weekly_weather.delete('area')
+
+    # shop = Shop.create!(owner_id: current_user.owner_id, city_code: city_code)
+
+    forecast_weather_codes = local_weekly_weather['weatherCodes']
+    forecast_pops = local_weekly_weather['pops']
+    forecast_weather_codes.zip(forecast_pops, weekly_dates) do |code, pop, date|
+      binding.irb
+      # shop.shop_weathers.create!(forecast_weather_code: code, forecast_pop: pop, date: Date.parse(date))
+    end
+
+    render json: local_weekly_weather
   end
 
   private
 
   def area_code
-    city_code = '1310800' # 江東区
+    area_response = Faraday.get('https://www.jma.go.jp/bosai/common/const/area.json')
+    area_response_json = JSON.parse(area_response.body)
+    offices_area_code = area_response_json['class10s'][local_code]['parent']
+    offices_area_code
+  end
+
+  def local_code
     area_response = Faraday.get('https://www.jma.go.jp/bosai/common/const/area.json')
     area_response_json = JSON.parse(area_response.body)
     class15s_area_code = area_response_json['class20s'][city_code]['parent']
     class10s_area_code = area_response_json['class15s'][class15s_area_code]['parent']
-    offices = area_response_json['class10s'][class10s_area_code]['parent']
-    offices
+    class10s_area_code
+  end
+
+  def city_code
+    city_code = '1310800' # 江東区
+    city_code
   end
 end
